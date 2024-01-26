7b6b433b103530519536e88cf6cc495f0c07db2c
Haoyang <heyemlee@gmail.com>
2024-01-26 01:17:34 -0800

    Remove node_modules and keys.js from version control

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    googleClientID: '683339588375-5isibqpucfl82em5lbs9suns6udc2q50.apps.googleusercontent.com',
-    googleClientSecret: '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'
-};
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys'); 
 const app = express();
 
-// clientID: 
-// clientSecret: 
-passport.use(new GoogleStrategy);
-app.get('/', (req, res)=>{
-    res.send({bye: 'buddy'});
-});
+//Config Google strategy with Passport.
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    //The redirected URL after completing the Google login process.
+    callbackURL: '/auth/google/callback'
+}, accessToken => {
+    //Callback function, receive the accessToken returned from Google.
+    console.log(accessToken );
+}));
 
-const PORT = process.env.PORT || 5000;
+//Create authentication route.  
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
